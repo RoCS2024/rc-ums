a5c95ae8f9e9a1f7b94ae6d91163e0fc99324bce
Josh <joshuagerome.roque@gmail.com>
2024-03-13 01:55:22 +0800

    Added Exceptions

--- user-management-application/src/main/java/com/user/management/data/connection/ConnectionHelper.java
+++ user-management-application/src/main/java/com/user/management/data/connection/ConnectionHelper.java
@@ -7,10 +7,10 @@ import java.sql.SQLException;
  * This is the Connection Helper.
  * */
 public class ConnectionHelper {
-    public static final String URL = "jdbc:oracle:thin:@localhost:1521:baemonville";
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:rogate";
     public static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
     public static final String username = "system";
-    public static final String password = "J03J16R11L27";
+    public static final String password = "Changeme0";
 
     public static Connection getConnection() throws RuntimeException {
         try {
