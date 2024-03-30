1352c3ea813847238e3c47115476f4116e717d35
Ram Kian Zedrick Ramirez <ram.kian.zedrick.ramirez@gmail.com>
2024-03-30 15:44:17 +0800

    Refactored RCPS-126 and RCPS-128 :User Management System: Fix EmployeeFacadeImplTest unit test and Fix UserFacadeImplTest unit test

--- src/test/java/com/user/management/appl/facade/user/Impl/UserFacadeImplTest.java
+++ src/test/java/com/user/management/appl/facade/user/Impl/UserFacadeImplTest.java
@@ -17,7 +17,6 @@ import java.util.Date;
 import java.util.List;
 
 import static com.user.management.data.connection.ConnectionHelper.username;
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
