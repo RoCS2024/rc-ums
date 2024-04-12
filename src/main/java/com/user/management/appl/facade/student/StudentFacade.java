33ceab0925f09af01fb448e8ede457c0d2a0af98
kylmrcdr123 <155501148+kylmrcdr123@users.noreply.github.com>
2024-04-12 18:23:25 +0800

    RCPS 152 AND 154 User Management System: Create Documentation for User Facade And Student Facade (#47)
     RCPS 152 AND 154 User Management System: Create Documentation for User Facade And Student Facade

--- src/main/java/com/user/management/appl/facade/student/StudentFacade.java
+++ src/main/java/com/user/management/appl/facade/student/StudentFacade.java
@@ -5,12 +5,29 @@ import com.user.management.appl.model.student.Student;
 
 import java.sql.SQLException;
 /**
- * This is the Student Facade.
- * */
+ * This is the interface to the Student Facade
+ */
 public interface StudentFacade {
+
+    /**
+     * This finds a student by their ID.
+     *
+     * @param studentId is the student ID to find in the database.
+     * @return student ID from the database
+     */
     Student findStudentById(String studentId);
+    /**
+     * This finds a student by their email.
+     *
+     * @param studentEmail is the student email to find in the database.
+     * @return student email from the database
+     */
     Student findStudentByEmail(String studentEmail);
-
+    /**
+     * This saves a new student in the database.
+     *
+     * @param student is the student to be saved.
+     * @return True if the student was successfully saved.
+     */
     boolean saveStudent(Student student);
-
 }
