f8abf4b722c6e905a8a11a1e85ab1cb8d8736e87
Kate Ann I. Amulong <amulongkateann@gmail.com>
2024-04-22 19:05:37 +0800

    RCPS-179:User Management System: Refactor ConnectionHelper

--- src/main/java/com/user/management/data/connection/ConnectionHelper.java
+++ src/main/java/com/user/management/data/connection/ConnectionHelper.java
@@ -1,27 +1,41 @@
 package com.user.management.data.connection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * This is the Connection Helper.
- * */
+ * The ConnectionHelper class connects to an Oracle database.
+ */
 public class ConnectionHelper {
-
-
-    public static final String URL = "jdbc:oracle:thin:@localhost:1521:rogate";
+    /** The connection URL. */
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:oracleDB";
+    /** The Oracle driver. */
     public static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    /** The username used to connect to the database. */
     public static final String username = "system";
+    /** The password used to connect to the database. */
     public static final String password = "Changeme0";
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConnectionHelper.class);
+
+    /**
+     * This method gets the connection from an Oracle database instance.
+     * */
     public static Connection getConnection() throws RuntimeException {
         try {
             Class.forName(ORACLE_DRIVER).newInstance();
             return DriverManager.getConnection(URL, username, password);
-        } catch (Exception ex) {
-            throw new RuntimeException("Error connecting to the database", ex);
-
+        } catch (ClassNotFoundException ex) {
+            LOGGER.error("Error has occurred. Driver not found." + ex.getMessage());
+        } catch (InstantiationException | IllegalAccessException ex) {
+            LOGGER.error("Error has occurred. Cannot create a database instance." + ex.getMessage());
+        } catch (SQLException ex) {
+            LOGGER.error("Error has occurred. Cannot connect to the database." + ex.getMessage());
         }
+        return null;
     }
 }
